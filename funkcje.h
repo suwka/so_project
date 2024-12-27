9f3001657c3b8e3300b968dd8d63a97d351862ce
Krzysztof Łęczycki <leczycki.krzysztoff@gmail.com>
2024-12-27 23:27:22 +0100

    zbedne komentarzez headera

--- funkcje.h
+++ funkcje.h
@@ -19,22 +19,11 @@
 #define MAX_POCZEKALNIA 10
 #define MAX_FOTELE 3
 
-// Tworzenie semafora
 int utworz_semafor(key_t klucz, int liczba_semaforow);
-
-// Usuwanie semafora
 void usun_semafor(int semafor_id);
-
-// Zwiększenie semafora
 void semafor_v(int semafor_id, int numer_semafora);
-
-// Zmniejszenie semafora
 void semafor_p(int semafor_id, int numer_semafora);
-
-// Ustawienie wartości semafora
 void inicjalizuj_semafor(int semafor_id, int numer_semafora, int wartosc);
-
-// Pobranie wartości semafora
 int pobierz_wartosc_semafora(int semafor_id, int numer_semafora);
 
-#endif // FUNKCJE_H
+#endif

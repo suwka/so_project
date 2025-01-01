ed398a35361166393c3c9fbc93b56d27fef5db92
Krzysztof Łęczycki <leczycki.krzysztoff@gmail.com>
2025-01-01 14:31:51 +0100

    build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+OUTPUT="salon"
+FILES="fryzjer.c funkcje.c klient.c main.c"
+LIBS="-lpthread"
+
+gcc -o $OUTPUT $FILES $LIBS
+
+if [ $? -eq 0 ]; then
+    echo "$OUTPUT"
+else
+    echo "blad"
+    exit 1
+fi
